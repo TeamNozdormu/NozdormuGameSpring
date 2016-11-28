4c28dd6b2a9c9a504f53e56c53e00547272ca8e2
ivanof <ivaylo.ivanof@gmail.com>
2016-11-28 22:03:31 +0200

    update terminal

--- src/main/java/com/nozdormu/terminal/Terminal.java
+++ src/main/java/com/nozdormu/terminal/Terminal.java
@@ -1,17 +1,27 @@
 package com.nozdormu.terminal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.nozdormu.gameobjects.GameImpl;
 import com.nozdormu.gameobjects.interfaces.Game;
+import com.nozdormu.service.PlayerService;
+import com.nozdormu.service.setting.GameSettingService;
+import com.nozdormu.service.setting.PlayerSettingService;
 
 @Component
 public class Terminal implements CommandLineRunner {
 	
+	@Autowired
+	private GameSettingService gameSettingService;
+	
+	@Autowired
+	private PlayerSettingService playerSettingService;
+	
 	@Override
 	public void run(String... arg0) throws Exception {			
-		Game game = new GameImpl();
+		Game game = new GameImpl(this.gameSettingService, this.playerSettingService);
 		game.start();                  
 	}
 }
