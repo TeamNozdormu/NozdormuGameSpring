757eeef8d2f663784b4e8f12e521a3140765609b
ivanof <ivaylo.ivanof@gmail.com>
2016-11-29 07:12:40 +0200

    add playerService and enemyService

--- src/main/java/com/nozdormu/terminal/Terminal.java
+++ src/main/java/com/nozdormu/terminal/Terminal.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import com.nozdormu.gameobjects.GameImpl;
 import com.nozdormu.gameobjects.interfaces.Game;
+import com.nozdormu.service.EnemyService;
 import com.nozdormu.service.PlayerService;
 import com.nozdormu.service.setting.GameSettingService;
 import com.nozdormu.service.setting.PlayerSettingService;
@@ -19,9 +20,19 @@ public class Terminal implements CommandLineRunner {
 	@Autowired
 	private PlayerSettingService playerSettingService;
 	
+	@Autowired
+    private PlayerService playerService;
+    
+    @Autowired
+    private EnemyService enemyService;
+	
 	@Override
 	public void run(String... arg0) throws Exception {			
-		Game game = new GameImpl(this.gameSettingService, this.playerSettingService);
+		Game game = new GameImpl(
+				this.gameSettingService, 
+				this.playerSettingService,
+				this.playerService,
+				this.enemyService);
 		game.start();                  
-	}
+	}	
 }
